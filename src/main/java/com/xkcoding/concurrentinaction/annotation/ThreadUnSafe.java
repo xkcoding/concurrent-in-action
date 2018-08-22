70d164d5b53e082aab8e05131a60d0346f71dc80
Yangkai.Shen <237497819@qq.com>
2018-08-22 22:54:16 +0800

    :sparkles: 增加几个注解

---
+++ src/main/java/com/xkcoding/concurrentinaction/annotation/ThreadUnSafe.java
@@ -0,0 +1,27 @@
+package com.xkcoding.concurrentinaction.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>
+ * 用于标记线程【不安全】的类
+ * </p>
+ *
+ * @package: com.xkcoding.concurrentinaction.annotation
+ * @description： 用于标记线程【不安全】的类
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/22 下午10:49
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface ThreadUnSafe {
+
+	String value() default "";
+
+}
