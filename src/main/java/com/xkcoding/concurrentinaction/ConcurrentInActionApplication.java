dee80674468c5e48e0d3563668c2ffc9716dbcc2
Yangkai.Shen <237497819@qq.com>
2018-08-22 22:43:55 +0800

    :tada: 初始化项目

---
+++ src/main/java/com/xkcoding/concurrentinaction/ConcurrentInActionApplication.java
@@ -0,0 +1,25 @@
+package com.xkcoding.concurrentinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * 并发编程实践学习启动类
+ * </p>
+ *
+ * @package: com.xkcoding.concurrentinaction
+ * @description： 并发编程实践学习启动类
+ * @author: yangkai.shen
+ * @date: Created in 2018/8/22 下午10:43
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@SpringBootApplication
+public class ConcurrentInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcurrentInActionApplication.class, args);
+	}
+}
