7929a3b138ac71009f6d2c87592a57f059a3a079
Yangkai.Shen <237497819@qq.com>
2018-09-21 20:57:08 +0800

    :sparkles: ThreadLocal 示例

---
+++ src/main/java/com/xkcoding/concurrentinaction/threadlocal/RequestHolder.java
@@ -0,0 +1,43 @@
+package com.xkcoding.concurrentinaction.threadlocal;
+
+/**
+ * <p>
+ * Request 持有器
+ * </p>
+ *
+ * @package: com.xkcoding.concurrentinaction.threadlocal
+ * @description: Request 持有器
+ * @author: yangkai.shen
+ * @date: Created in 2018/9/21 下午8:50
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+public class RequestHolder {
+	private final static ThreadLocal<Long> REQUEST_HOLDER = new ThreadLocal<>();
+
+	/**
+	 * 为当前request添加一个id
+	 *
+	 * @param id 值
+	 */
+	public static void add(Long id) {
+		REQUEST_HOLDER.set(id);
+	}
+
+	/**
+	 * 获取当前request保存的id
+	 *
+	 * @return 当前request保存的id
+	 */
+	public static Long getId() {
+		return REQUEST_HOLDER.get();
+	}
+
+	/**
+	 * 清空缓存
+	 */
+	public static void remove() {
+		REQUEST_HOLDER.remove();
+	}
+}
---
+++ src/main/java/com/xkcoding/concurrentinaction/threadlocal/ThreadLocalController.java
@@ -0,0 +1,31 @@
+package com.xkcoding.concurrentinaction.threadlocal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 测试ThreadLocal
+ * </p>
+ *
+ * @package: com.xkcoding.concurrentinaction.threadlocal
+ * @description: 测试ThreadLocal
+ * @author: yangkai.shen
+ * @date: Created in 2018/9/21 下午8:52
+ * @copyright: Copyright (c) 2018
+ * @version: V1.0
+ * @modified: yangkai.shen
+ */
+@RestController
+@RequestMapping("/threadLocal")
+public class ThreadLocalController {
+
+	/**
+	 * 测试ThreadLocal保存的值
+	 */
+	@GetMapping("/test")
+	public Long test() {
+		return RequestHolder.getId();
+	}
+}
